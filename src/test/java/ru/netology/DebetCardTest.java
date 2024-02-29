97330ee296a243eb7ec8a8730ac33f83805ecfd3
Arina Kotysheva <kotysheva.arina@gmail.com>
2024-02-29 23:03:59 +0700

    correcting errors

--- src/test/java/ru/netology/DebetCardTest.java
+++ src/test/java/ru/netology/DebetCardTest.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class AppOrderPositiveTest {
+class DebetCardTest {
     WebDriver driver;
 
     @BeforeAll
