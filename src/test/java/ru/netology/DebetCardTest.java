30dc4788a1156ca732be86006ee1c53e63d3b3c1
Arina Kotysheva <kotysheva.arina@gmail.com>
2024-03-03 13:43:02 +0700

    correcting gradle

--- src/test/java/ru/netology/DebetCardTest.java
+++ src/test/java/ru/netology/DebetCardTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DebetCardTest {
